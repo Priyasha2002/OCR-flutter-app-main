88ecbb4a7d89bf45be77d6aa7595a70bb6ef3233
Priyasha2002 <ghoshpriyasha29@gmail.com>
2024-02-28 22:02:52 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:ocr/Screen/recognization_page.dart';
+import 'package:ocr/Utils/image_cropper_page.dart';
+import 'package:ocr/Utils/image_picker_class.dart';
+import 'package:ocr/Widgets/modal_dialog.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: const Center(
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          imagePickerModal(context, onCameraTap: () {
+            log("Camera");
+            pickImage(source: ImageSource.camera).then((value) {
+              if (value != '') {
+                imageCropperView(value, context).then((value) {
+                  if (value != '') {
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (_) => RecognizePage(
+                          path: value,
+                        ),
+                      ),
+                    );
+                  }
+                });
+              }
+            });
+          }, onGalleryTap: () {
+            log("Gallery");
+            pickImage(source: ImageSource.gallery).then((value) {
+              if (value != '') {
+                imageCropperView(value, context).then((value) {
+                  if (value != '') {
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (_) => RecognizePage(
+                          path: value,
+                        ),
+                      ),
+                    );
+                  }
+                });
+              }
+            });
+          });
+
+        },
+
+        tooltip: 'Increment',
+        label: const Text("Scan photo"),
+      ),
+    // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
